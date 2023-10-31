e6c640900f9b1021ba26e830aa17e73b2ff31baa
fawwasaldy <fawwas.aldy2111@gmail.com>
2023-10-31 08:38:13 +0700

    added html and js

---
+++ calculator.js
@@ -0,0 +1,10 @@
+const { createApp, ref } = Vue
+createApp({
+    data() {
+        return {
+            bil1: '',
+            bil2: '',
+            hasil: '',
+        }
+    }
+}).mount('#app')
---
+++ list.js
@@ -0,0 +1,21 @@
+const { createApp, ref } = Vue
+createApp({
+    data() {
+        return {
+            python_is_trash: [
+                {
+                    nama: 'suka',
+                    kelas: 'paitan',
+                    hobi: 'bim bim'
+                },
+                {
+                    nama: 'blyadd',
+                    kelas: 'si plas plas',
+                    hobi: 'bam bam'
+                }
+            ]
+        }
+    }
+}).mount('#app2')
+
+console.log(this.python_is_trash);
